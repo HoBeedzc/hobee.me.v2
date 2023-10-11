785ac539652309e9292c99a2d5316e698d2531b0
Zequn Liu <lzqpublic@163.com>
2023-10-11 16:21:16 +0000

    fix vercel deploy

--- vercel_build.sh
+++ vercel_build.sh
@@ -7,6 +7,9 @@ yum install go
 # install hugo lastest version
 git clone https://github.com/gohugoio/hugo.git -b release-0.119.0 hugo-source
 cd hugo-source
+git checkout -b release-0.119.0
+git branch
+exit
 go build
 
 cd ..
